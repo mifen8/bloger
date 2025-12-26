a4082b2f42fefa6a40398d832b7edc1c573492a7
mifen8 <crimsonmi51@gmail.com>
2025-12-26 21:51:50 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "yCENzh's Blog",
-	subtitle: "爱你所爱~",
+	title: "Mifener's Blog",
+	subtitle: "welcome to share tech",
 	themeColor: {
 		hue: 275, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
